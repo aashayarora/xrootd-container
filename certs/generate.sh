ccaddd17f76edd67659c1e874d87b9716a90d77e
aaarora <aaarora@ucsd.edu>
2022-03-02 20:16:36 -0800

    updating to use sense image

---
+++ certs/generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export PASSPHRASE=123456
+
+HOSTNAME=$1 # IPv6 address (no brackets, no capitals)
+BASEDIR=$2
+
+openssl req -new -newkey rsa:2048 -nodes \
+    -keyout $BASEDIR/hostcert.key.pem -subj "/CN=$HOSTNAME" > $BASEDIR/hostcert.csr
+openssl x509 -req -days 9999 -CAcreateserial \
+    -extfile <(printf "subjectAltName=DNS:$HOSTNAME,IP:$HOSTNAME") \
+    -in $BASEDIR/hostcert.csr \
+    -CA rucio_ca.pem -CAkey rucio_ca.key.pem \
+    -out $BASEDIR/hostcert.pem \
+    -passin env:PASSPHRASE
+
+chmod 0400 $BASEDIR/*key*
