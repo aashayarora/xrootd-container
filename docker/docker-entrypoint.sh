82cddd146b3565c2797f8e5c7d6335bd3cf9f3f7
aaarora <aaarora@ucsd.edu>
2022-08-08 13:06:42 -0700

    updated to general image for https tpc

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Allow the derived images to run any additional runtime customizations
+shopt -s nullglob
+for x in /etc/osg/image-init.d/*.sh; do source "$x"; done
+shopt -u nullglob
+
+chmod go-w /etc/cron.*/* 2>/dev/null || :
+
+# Now we can actually start the supervisor
+exec /usr/bin/supervisord -c /etc/supervisord.conf
